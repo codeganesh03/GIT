1946cac21fb8ed8b9bbad53d5d2402b75b46b411
codeganesh03 <coderaja03@gmail.com>
2026-08-13 23:17:08 +0530

    initial commit

---
+++ add.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("This is a second file named add");
+    return 0;
+}
---
+++ sum.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("Sample for git");
+    return 0;
+}
